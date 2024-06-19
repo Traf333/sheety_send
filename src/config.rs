f4e95426490a52216f49e70d52b02f08bd0af905
Igor Trofimov <igor@litentry.com>
2024-06-19 12:52:57 +0200

    add readme

--- src/config.rs
+++ src/config.rs
@@ -1,20 +1,22 @@
-// use std::env;
+use dotenv::dotenv;
+use std::env;
 
 pub struct Config {
     pub spreadsheet_id: String,
-    // pub recipient_email: String,
-    // pub smtp_username: String,
-    // pub smtp_password: String,
+    pub recipient_email: String,
+    pub smtp_username: String,
+    pub smtp_password: String,
 }
 
 impl Config {
     pub fn from_env() -> Self {
+        dotenv().ok();
+
         Config {
-            spreadsheet_id: "1xS2W2HoyAJ39W-KPuPgJ6gWjOpBzoYKiM03yhswW3P0".to_string(),
-            // spreadsheet_id: env::var("SPREADSHEET_URL").expect("SPREADSHEET_URL not set"),
-            // recipient_email: env::var("RECIPIENT_EMAIL").expect("RECIPIENT_EMAIL not set"),
-            // smtp_username: env::var("SMTP_USERNAME").expect("SMTP_USERNAME not set"),
-            // smtp_password: env::var("SMTP_PASSWORD").expect("SMTP_PASSWORD not set"),
+            spreadsheet_id: env::var("SPREADSHEET_ID").expect("SPREADSHEET_ID not set"),
+            recipient_email: env::var("RECIPIENT_EMAIL").expect("RECIPIENT_EMAIL not set"),
+            smtp_username: env::var("SMTP_USERNAME").expect("SMTP_USERNAME not set"),
+            smtp_password: env::var("SMTP_PASSWORD").expect("SMTP_PASSWORD not set"),
         }
     }
 }
